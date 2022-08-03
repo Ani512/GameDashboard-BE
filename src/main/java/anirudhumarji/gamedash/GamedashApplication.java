91e06275d2c541f49d547b217d46e8571551b8ab
Anirudh Umarji <anirudh.umarji@gmail.com>
2022-08-03 16:00:28 -0400

    setup

---
+++ src/main/java/anirudhumarji/gamedash/GamedashApplication.java
@@ -0,0 +1,13 @@
+package anirudhumarji.gamedash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamedashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamedashApplication.class, args);
+	}
+
+}
---
+++ src/main/java/anirudhumarji/gamedash/ServletInitializer.java
@@ -0,0 +1,13 @@
+package anirudhumarji.gamedash;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(GamedashApplication.class);
+	}
+
+}
