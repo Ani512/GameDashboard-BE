d0181d7824977f660052194603e8589dab254076
Anirudh Umarji <55306116+Ani512@users.noreply.github.com>
2022-08-04 08:53:50 -0500

    create Base URL Data (#4)

---
+++ src/main/java/anirudhumarji/gamedash/services/BaseUrlService.java
@@ -0,0 +1,16 @@
+package anirudhumarji.gamedash.services;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class BaseUrlService {
+
+    public static Map<String, String> endPointList() {
+        HashMap<String, String> endPoints = new HashMap<>();
+
+        endPoints.put("/", "baseURL which returns list of all Endpoints");
+        endPoints.put("/version", "returns the version of the API being used");
+
+        return endPoints;
+    }
+}
